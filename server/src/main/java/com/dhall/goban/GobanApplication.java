f1213d43606e7f7837f8efb08977a9dd1fb5b5ac
Derrick Hall <derrick.hall@iongroup.com>
2018-08-09 17:53:32 -0400

    Enable CORS and make simple request to server from client

--- server/src/main/java/com/dhall/goban/GobanApplication.java
+++ server/src/main/java/com/dhall/goban/GobanApplication.java
@@ -3,10 +3,14 @@ package com.dhall.goban;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import ru.vyarus.dropwizard.guice.GuiceBundle;
 
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.EnumSet;
 
 public class GobanApplication extends Application<GobanConfiguration> {
 
@@ -29,6 +33,18 @@ public class GobanApplication extends Application<GobanConfiguration> {
     @Override
     public void run(final GobanConfiguration configuration,
                     final Environment environment) {
+        final FilterRegistration.Dynamic cors =
+                environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+        // Configure CORS parameters
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, "X-Requested-With,Content-Type,Accept,Origin,Authorization");
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+        cors.setInitParameter(CrossOriginFilter.ALLOW_CREDENTIALS_PARAM, "true");
+
+        // Add URL mapping
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+
         DateFormat exampleDateFormat = new SimpleDateFormat(configuration.getDateFormat());
         environment.getObjectMapper().setDateFormat(exampleDateFormat);
     }
